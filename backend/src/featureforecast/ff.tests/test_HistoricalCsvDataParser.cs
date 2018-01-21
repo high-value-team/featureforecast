0a8b85e34ad011aa1f3fb1db7859f274c34860e5
Ralf Westphal <info@ralfw.de>
2018-01-21 12:18:10 +0100

    scaffolding tests removed

--- backend/src/featureforecast/ff.tests/test_HistoricalCsvDataParser.cs
+++ backend/src/featureforecast/ff.tests/test_HistoricalCsvDataParser.cs
@@ -53,56 +53,5 @@ namespace ff.tests
                 },
                 result);
         }
-        
-        [Test]
-        public void Deal_with_whitespace2()
-        {
-            var result = HistoricalCsvDataParser.Parse("2");
-            Equalidator.AreEqual(new[] {
-                    new History.Datapoint{Value=2f,Tags=new string[0]}
-                },
-                result);
-        }
-        
-        [Test]
-        public void Split_into_lines() {
-            var result = HistoricalCsvDataParser.Split_into_lines("1\n2\r\n3\n\r4");
-            Assert.AreEqual(new[]{"1","2","3","4"}, result);
-        }
-        
-        [Test]
-        public void Empty_lines_are_removed() {
-            var result = HistoricalCsvDataParser.Split_into_lines("1\n\n2\n \n3");
-            Assert.AreEqual(new[]{"1","2","3"}, result);
-        }
-
-        [Test]
-        public void Create_records_with_trimmed_columns()
-        {
-            var result = HistoricalCsvDataParser.Split_columns(new[] {" a ; b ", "x; y , z ", "s"});
-            Assert.AreEqual(new[]{new[]{"a","b"}, new[]{"x","y , z"}, new[]{"s", ""}}, result);
-        }
-
-        [Test]
-        public void Create_datapoints()
-        {
-            var result = HistoricalCsvDataParser.Map_to_datapoints(new[]
-            {
-                new[]{" 3.14 ", " a , b "},
-                new[]{"42,0", "c"},
-                new[]{"1", ",d"},
-                new[]{"2", "d,"},
-                new[]{"3", ""},
-            });
-            
-            Equalidator.AreEqual(new[] {
-                    new History.Datapoint{Value=3.14f,Tags=new[]{"a","b"}},
-                    new History.Datapoint{Value=42f,Tags=new[]{"c"}},
-                    new History.Datapoint{Value=1f,Tags=new[]{"d"}},
-                    new History.Datapoint{Value=2f,Tags=new[]{"d"}},
-                    new History.Datapoint{Value=3f,Tags=new string[0]}
-                },
-                result);
-        }
     }
 }
