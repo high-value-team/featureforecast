ab0f580ec6dbd663e6ca11985748b4964b71e90d
Florian Boehmak <florian@fnbk.cc>
2018-04-05 12:45:18 +0200

    rename redux/project.js into Redux.js

--- frontend/src/src/containers/HistoryContainer.js
+++ frontend/src/src/containers/HistoryContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import HistoryPage from '../components/HistoryPage';
 import {bindActionCreators} from 'redux';
 import {connect} from 'react-redux';
-import * as boxActionCreators from '../redux/project';
+import * as boxActionCreators from '../Redux';
 import PropTypes from 'prop-types';
 import Typography from 'material-ui/Typography';
 
