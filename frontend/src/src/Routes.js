655b09bb1b64c701b1ffd7bdbc2a0e80735819b4
Florian Boehmak <florian@fnbk.cc>
2018-05-10 18:28:05 +0200

    add imprint

--- frontend/src/src/Routes.js
+++ frontend/src/src/Routes.js
@@ -4,6 +4,7 @@ import MainContainer from './containers/MainContainer';
 import CreateHistoryContainer from './containers/CreateHistoryContainer';
 import HistoryContainer from './containers/HistoryContainer';
 import ForecastContainer from "./containers/ForecastContainer";
+import ImprintContainer from "./containers/ImprintContainer";
 
 export default function getRoutes (history) {
     return (
@@ -11,6 +12,7 @@ export default function getRoutes (history) {
             <Route path="/" component={MainContainer}>
                 <Route path="/:projectID/history" component={HistoryContainer} />
                 <Route path="/:projectID/forecast" component={ForecastContainer} />
+                <Route path="/imprint" component={ImprintContainer} />
 
                 <IndexRoute component={CreateHistoryContainer}/>
             </Route>
